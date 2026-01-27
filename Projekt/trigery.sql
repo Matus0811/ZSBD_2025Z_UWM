0ac037b77d3a38f9a4703b08783e56e2dbd5cb0c
Kamil Matusiak <matusiak.kamil@outlook.com>
2026-01-27 14:01:36 +0100

    Dodanie triggera archiwizującego usunięte bilety

---
+++ Projekt/trigery.sql
@@ -0,0 +1,25 @@
+------------------------------------------------------------
+-- Trigger archiwizujący usunięte bilety
+------------------------------------------------------------
+CREATE OR REPLACE TRIGGER trg_archive_ticket
+BEFORE DELETE ON tickets
+FOR EACH ROW
+BEGIN
+  INSERT INTO tickets_archive (
+    ticket_id,
+    seance_id,
+    customer_id,
+    price,
+    sale_date,
+    deleted_at
+  )
+  VALUES (
+    :OLD.ticket_id,
+    :OLD.seance_id,
+    :OLD.customer_id,
+    :OLD.price,
+    :OLD.sale_date,
+    SYSDATE
+  );
+END;
+/
