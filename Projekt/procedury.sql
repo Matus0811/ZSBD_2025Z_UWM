2068ce7a17875294c3be743a505ca2a2169f1021
Kamil Matusiak <matusiak.kamil@outlook.com>
2026-01-29 17:04:18 +0100

    Dodanie walidacji danych i obsługi wyjątków do procedur

--- Projekt/procedury.sql
+++ Projekt/procedury.sql
@@ -1,20 +1,34 @@
 ------------------------------------------------------------
--- Procedura sprzedaży biletu
+-- Procedura sprzedaży biletu 
 ------------------------------------------------------------
 CREATE OR REPLACE PROCEDURE sell_ticket (
   p_seance_id   IN NUMBER,
   p_customer_id IN NUMBER,
   p_price       IN NUMBER
 ) AS
+  e_invalid_price EXCEPTION;
 BEGIN
+  IF p_price <= 0 THEN
+    RAISE e_invalid_price;
+  END IF;
+
   INSERT INTO tickets (seance_id, customer_id, price)
   VALUES (p_seance_id, p_customer_id, p_price);
 
   COMMIT;
+  DBMS_OUTPUT.PUT_LINE('Sprzedano bilet pomyślnie.');
+
+EXCEPTION
+  WHEN e_invalid_price THEN
+    DBMS_OUTPUT.PUT_LINE('BŁĄD: Cena biletu musi być większa od zera!');
+  WHEN OTHERS THEN
+    DBMS_OUTPUT.PUT_LINE('Wystąpił błąd bazy danych: ' || SQLERRM);
+    ROLLBACK;
 END;
 /
+
 ------------------------------------------------------------
--- Procedura usuwania biletu
+-- Procedura usuwania biletu (DELETE + Informacja zwrotna)
 ------------------------------------------------------------
 CREATE OR REPLACE PROCEDURE delete_ticket (
   p_ticket_id IN NUMBER
@@ -23,6 +37,42 @@ BEGIN
   DELETE FROM tickets
   WHERE ticket_id = p_ticket_id;
 
-  COMMIT;
+  IF SQL%ROWCOUNT = 0 THEN
+    DBMS_OUTPUT.PUT_LINE('Nie znaleziono biletu o ID: ' || p_ticket_id);
+  ELSE
+    COMMIT;
+    DBMS_OUTPUT.PUT_LINE('Usunięto bilet o ID: ' || p_ticket_id);
+  END IF;
+
+EXCEPTION
+  WHEN OTHERS THEN
+    DBMS_OUTPUT.PUT_LINE('Błąd podczas usuwania: ' || SQLERRM);
+    ROLLBACK;
+END;
+/
+
+------------------------------------------------------------
+-- Procedura aktualizacji emaila klienta 
+------------------------------------------------------------
+CREATE OR REPLACE PROCEDURE update_customer_email (
+  p_customer_id IN NUMBER,
+  p_new_email   IN VARCHAR2
+) AS
+BEGIN
+  UPDATE customers
+  SET email = p_new_email
+  WHERE customer_id = p_customer_id;
+
+  IF SQL%ROWCOUNT = 0 THEN
+    DBMS_OUTPUT.PUT_LINE('Nie znaleziono klienta o ID: ' || p_customer_id);
+  ELSE
+    COMMIT;
+    DBMS_OUTPUT.PUT_LINE('Zaktualizowano email dla klienta ID: ' || p_customer_id);
+  END IF;
+
+EXCEPTION
+  WHEN OTHERS THEN
+    DBMS_OUTPUT.PUT_LINE('Błąd aktualizacji: ' || SQLERRM);
+    ROLLBACK;
 END;
 /
